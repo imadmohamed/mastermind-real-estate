8b72cda104409800e007a25f092717a294a25d2f
imad mohamed <abmohammedimad00@gmail.com>
2025-04-25 08:53:39 +0400

    map change

--- client/src/components/ArcGISMapNext.tsx
+++ client/src/components/ArcGISMapNext.tsx
@@ -168,260 +168,174 @@
 // this is the real code
 
 
-// 'use client';
-
-// import { useEffect, useRef } from 'react';
-// import Script from 'next/script';
-// import ArcMap from "../components/arc-map"
-
-// // Type declarations for custom ArcGIS web components
-// declare global {
-//   interface Window {
-//     esriConfig: any;
-//     require: any;
-//   }
-
-//   namespace JSX {
-//     interface IntrinsicElements {
-//       'arcgis-map': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-//         center?: string;
-//         zoom?: string;
-//         ref?: React.Ref<HTMLElement>;
-//         map?: any; // Add this property to support dynamic map assignment
-//       };
-//       'arcgis-zoom': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-//         position?: string;
-//       };
-//     }
-//   }
-// }
-
-// const ArcGISMapNext = ({markerValues}:any) => {
-//   const mapRef = useRef<HTMLElement | null>(null);
-
-//   useEffect(() => {
-//     if (typeof window !== 'undefined') {
-//       window.esriConfig = {
-//         apiKey: 'AAPTxy8BH1VEsoebNVZXo8HurDYlwQfsui3QcFubojHCp0BRFrQ8g5utzySCpv2At8MclB-Re8Ur0PBmv-mX_QGt9sZerw6I8rLkwFBYRwGPez0Kkii1ljdT7xSwv0oNrHiq2UmX5119mxVo-Es2LpS23G5RZjXBBuxWCMOsi2OmMUMhWIzjTDpFm9vh_F5dgHeqpzenyeCpwHLAJmZh1I4D8Qw3J7S7dKT9jQ5Y7DOmlIE.AT1_5KOrYQj0',
-//       };
-//     }
-//   }, []);
-
-//   const handleArcGISLoad = () => {
-//     if (typeof window !== 'undefined' && window.require) {
-//       window.require(
-//         ['esri/Graphic', 'esri/Map', 'esri/layers/GraphicsLayer'],
-//         (Graphic: any, Map: any, GraphicsLayer: any) => {
-//           const arcgisMap = document.querySelector('arcgis-map') as any;
-//           if (!arcgisMap) return;
-
-//           const graphicsLayer = new GraphicsLayer();
-
-//           arcgisMap.map = new Map({
-//             basemap: 'arcgis/topographic',
-//             layers: [graphicsLayer],
-//           });
-
-//           // Point
-//           const point = {
-//             type: 'point',
-//             longitude: 0,
-//             latitude: 0,
-//           };
-
-//           const simpleMarkerSymbol = {
-//             type: 'simple-marker',
-//             color: [226, 119, 40],
-//             outline: {
-//               color: [255, 255, 255],
-//               width: 1,
-//             },
-//           };
-
-//           const pointGraphic = new Graphic({
-//             geometry: point,
-//             symbol: simpleMarkerSymbol,
-//           });
-//           graphicsLayer.add(pointGraphic);
-
-//           // Polyline
-//           const polyline = {
-//             type: 'polyline',
-//             paths: [
-//               [55.296249, 25.2048],
-//               [55.296249, 25.2048],
-//               [55.296249, 25.2048],
-//             ],
-//           };
-
-//           const simpleLineSymbol = {
-//             type: 'simple-line',
-//             color: [226, 119, 40],
-//             width: 2,
-//           };
-
-//           const polylineGraphic = new Graphic({
-//             geometry: polyline,
-//             symbol: simpleLineSymbol,
-//           });
-//           graphicsLayer.add(polylineGraphic);
-
-//           // Polygon
-//           const polygon = {
-//             type: 'polygon',
-//             rings:markerValues,
-//            };
-
-          
-
-//           const simpleFillSymbol = {
-//             type: 'simple-fill',
-//             color: [227, 139, 79, 0.8],
-//             outline: {
-//               color: [255, 255, 255],
-//               width: 1,
-//             },
-//           };
-
-//           const popupTemplate = {
-//             title: '{Master Mind Real Estate}',
-//             content: '{Description}',
-//           };
-
-//           const attributes = {
-//             Name: 'Graphic',
-//             Description: 'I am a polygon',
-//           };
-
-//           const polygonGraphic = new Graphic({
-//             geometry: polygon,
-//             symbol: simpleFillSymbol,
-//             attributes,
-//             popupTemplate,
-//           });
-
-//           graphicsLayer.add(polygonGraphic);
-//         }
-//       );
-//     }
-//   };
-
-//   return (
-//     <>
-    
-      
-//       <Script
-//         src="https://js.arcgis.com/map-components/4.32/arcgis-map-components.esm.js"
-//         type="module"
-//         strategy="afterInteractive"
-//       />
-//       <Script
-//         src="https://js.arcgis.com/4.32/"
-//         strategy="afterInteractive"
-//         onLoad={handleArcGISLoad}
-//       />
-
-//       <style jsx global>{`
-//         @import url('https://js.arcgis.com/4.32/esri/themes/light/main.css');
-//         arcgis-map {
-//           width: 100%;
-//           height: 100vh;
-//           display: block;
-//           margin: 0;
-//           padding: 0;
-//         }
-//       `}</style>
-//         <ArcMap/>
-     
-//     </>
-//   );
-// };
-
-// export default ArcGISMapNext;
-
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import Script from 'next/script';
+import ArcMap from "../components/arc-map"
 
+// Type declarations for custom ArcGIS web components
 declare global {
   interface Window {
     esriConfig: any;
     require: any;
   }
+
+  namespace JSX {
+    interface IntrinsicElements {
+      'arcgis-map': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        center?: string;
+        zoom?: string;
+        ref?: React.Ref<HTMLElement>;
+        map?: any; // Add this property to support dynamic map assignment
+      };
+      'arcgis-zoom': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        position?: string;
+      };
+    }
+  }
 }
 
-interface ArcGISMapNextProps {
-  markerValues: [number, number][];
-  apiKey?: string;
-}
+const ArcGISMapNext = ({markerValues}:any) => {
+  const mapRef = useRef<HTMLElement | null>(null);
 
-export default function ArcGISMapNext({ 
-  markerValues = [], 
-  apiKey = 'AAPTxy8BH1VEsoebNVZXo8HurDYlwQfsui3QcFubojHCp0BRFrQ8g5utzySCpv2At8MclB-Re8Ur0PBmv-mX_QGt9sZerw6I8rLkwFBYRwGPez0Kkii1ljdT7xSwv0oNrHiq2UmX5119mxVo-Es2LpS23G5RZjXBBuxWCMOsi2OmMUMhWIzjTDpFm9vh_F5dgHeqpzenyeCpwHLAJmZh1I4D8Qw3J7S7dKT9jQ5Y7DOmlIE.AT1_5KOrYQj0' 
-}: ArcGISMapNextProps) {
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      window.esriConfig = { apiKey };
+      window.esriConfig = {
+        apiKey: 'AAPTxy8BH1VEsoebNVZXo8HurDYlwQfsui3QcFubojHCp0BRFrQ8g5utzySCpv2At8MclB-Re8Ur0PBmv-mX_QGt9sZerw6I8rLkwFBYRwGPez0Kkii1ljdT7xSwv0oNrHiq2UmX5119mxVo-Es2LpS23G5RZjXBBuxWCMOsi2OmMUMhWIzjTDpFm9vh_F5dgHeqpzenyeCpwHLAJmZh1I4D8Qw3J7S7dKT9jQ5Y7DOmlIE.AT1_5KOrYQj0',
+      };
     }
-  }, [apiKey]);
+  }, []);
 
-  useEffect(() => {
-    if (typeof window !== 'undefined' && window.require && markerValues.length > 0) {
-      window.require([
-        'esri/Map',
-        'esri/views/MapView',
-        'esri/Graphic',
-        'esri/layers/GraphicsLayer',
-        'esri/geometry/Polygon'
-      ], (Map: any, MapView: any, Graphic: any, GraphicsLayer: any, Polygon: any) => {
-        const map = new Map({ basemap: 'streets-navigation-vector' });
-        const view = new MapView({
-          container: 'viewDiv',
-          map: map,
-          center: [markerValues[0][0], markerValues[0][1]],
-          zoom: 14
-        });
+  const handleArcGISLoad = () => {
+    if (typeof window !== 'undefined' && window.require) {
+      window.require(
+        ['esri/Graphic', 'esri/Map', 'esri/layers/GraphicsLayer'],
+        (Graphic: any, Map: any, GraphicsLayer: any) => {
+          const arcgisMap = document.querySelector('arcgis-map') as any;
+          if (!arcgisMap) return;
 
-        const graphicsLayer = new GraphicsLayer();
-        map.add(graphicsLayer);
+          const graphicsLayer = new GraphicsLayer();
 
-        if (markerValues.length >= 3) {
-          const polygon = {
-            type: 'polygon',
-            rings: [markerValues],
-            spatialReference: { wkid: 4326 }
+          arcgisMap.map = new Map({
+            basemap: 'arcgis/topographic',
+            layers: [graphicsLayer],
+          });
+
+          // Point
+          const point = {
+            type: 'point',
+            longitude: 0,
+            latitude: 0,
           };
 
-          const fillSymbol = {
+          const simpleMarkerSymbol = {
+            type: 'simple-marker',
+            color: [226, 119, 40],
+            outline: {
+              color: [255, 255, 255],
+              width: 1,
+            },
+          };
+
+          const pointGraphic = new Graphic({
+            geometry: point,
+            symbol: simpleMarkerSymbol,
+          });
+          graphicsLayer.add(pointGraphic);
+
+          // Polyline
+          const polyline = {
+            type: 'polyline',
+            paths: [
+              [55.296249, 25.2048],
+              [55.296249, 25.2048],
+              [55.296249, 25.2048],
+            ],
+          };
+
+          const simpleLineSymbol = {
+            type: 'simple-line',
+            color: [226, 119, 40],
+            width: 2,
+          };
+
+          const polylineGraphic = new Graphic({
+            geometry: polyline,
+            symbol: simpleLineSymbol,
+          });
+          graphicsLayer.add(polylineGraphic);
+
+          // Polygon
+          const polygon = {
+            type: 'polygon',
+            rings:markerValues,
+           };
+
+          
+
+          const simpleFillSymbol = {
             type: 'simple-fill',
             color: [227, 139, 79, 0.8],
             outline: {
               color: [255, 255, 255],
-              width: 1
-            }
+              width: 1,
+            },
+          };
+
+          const popupTemplate = {
+            title: '{Master Mind Real Estate}',
+            content: '{Description}',
+          };
+
+          const attributes = {
+            Name: 'Graphic',
+            Description: 'I am a polygon',
           };
 
           const polygonGraphic = new Graphic({
             geometry: polygon,
-            symbol: fillSymbol
+            symbol: simpleFillSymbol,
+            attributes,
+            popupTemplate,
           });
 
           graphicsLayer.add(polygonGraphic);
-          view.goTo(polygonGraphic);
         }
-      });
+      );
     }
-  }, [markerValues]);
+  };
 
   return (
     <>
+    
+      
       <Script
-        src="https://js.arcgis.com/4.28/"
-        strategy="lazyOnload"
-        onLoad={() => console.log('ArcGIS JS API loaded')}
+        src="https://js.arcgis.com/map-components/4.32/arcgis-map-components.esm.js"
+        type="module"
+        strategy="afterInteractive"
       />
-      <div id="viewDiv" className="w-full h-[500px]" />
+      <Script
+        src="https://js.arcgis.com/4.32/"
+        strategy="afterInteractive"
+        onLoad={handleArcGISLoad}
+      />
+
+      <style jsx global>{`
+        @import url('https://js.arcgis.com/4.32/esri/themes/light/main.css');
+        arcgis-map {
+          width: 100%;
+          height: 100vh;
+          display: block;
+          margin: 0;
+          padding: 0;
+        }
+      `}</style>
+        <ArcMap/>
+     
     </>
   );
-}
+};
+
+export default ArcGISMapNext;
+
